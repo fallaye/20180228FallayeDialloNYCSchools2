53265f2bad0477677755fa7f1d9949d362072cc4
Fallaye <leger.leger20@gmail.com>
2018-03-01 15:18:11 -0500

    Initial commit

---
+++ app/src/main/java/virtusa/com/a20180228fallayediallonycschools/MainActivity.java
@@ -0,0 +1,13 @@
+package virtusa.com.a20180228fallayediallonycschools;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
